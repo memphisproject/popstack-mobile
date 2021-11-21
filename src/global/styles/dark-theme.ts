9dd0c8b3f2b9aed883413ebbbbcaa9b5f0182487
m3k3r1 <m3k3r1@gmail.com>
2021-11-21 16:02:48 +0100

    dark theme colors setup

---
+++ src/global/styles/dark-theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    background: '#000000',
+    shape: '#323738',
+    shape_light: 'rgba(0,0,69,0.2)',
+    text: '#FFFFFF',
+    text_light: '#B0B0B0',
+    text_reverse: '#F9F9F9',
+    accent_color: '#28A9E0',
+
+    accent_light1: 'rgba(18,164,84,0.2)',
+    accent_light2: 'rgba(231,63,91,0.2)',
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+};
