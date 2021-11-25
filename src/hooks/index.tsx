8bf4ab1100be252406ab298d6826c9b30e933dcb
m3k3r1 <m3k3r1@gmail.com>
2021-11-25 12:00:31 +0100

    add children to provider

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,9 +1,14 @@
+import type { ReactNode } from 'react';
 import React from 'react';
 
 import { TabsProvider } from './useTabs';
 
-const AppProvider: React.FC = () => {
-  return <TabsProvider />;
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  return <TabsProvider>{children}</TabsProvider>;
 };
 
 export default AppProvider;
